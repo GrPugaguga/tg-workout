2b41bf504ee2c81b3d97d3acb11a4e6de91b6578
GrPugaguga <pugaguga@mail.ru>
2026-03-22 15:57:41 +0500

    fix(frontend): fix vercel bug

--- apps/frontend/components/TelegramTheme.tsx
+++ apps/frontend/components/TelegramTheme.tsx
@@ -2,9 +2,18 @@
 
 import { useEffect } from "react";
 
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp: {
+        colorScheme: "light" | "dark";
+      };
+    };
+  }
+}
+
 export function TelegramTheme() {
   useEffect(() => {
-
     const tg = window.Telegram?.WebApp;
     if (tg?.colorScheme === "dark") {
       document.documentElement.classList.add("dark");
