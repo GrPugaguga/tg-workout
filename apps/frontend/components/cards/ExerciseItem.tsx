62125169467cab1d5038b8f7d8d6ca15dc6b90ae
GrPugaguga <pugaguga@mail.ru>
2026-03-22 16:23:56 +0500

    fix(frontend): text color fix

--- apps/frontend/components/cards/ExerciseItem.tsx
+++ apps/frontend/components/cards/ExerciseItem.tsx
@@ -8,7 +8,7 @@ export function ExerciseItem({title, maxVal, onClick}: {title: string, maxVal: n
         >
             <div className="flex flex-col gap-4">
                 <span className=" font-medium text-[16px] leading-5.5 text-txt ">{title}</span>
-                { maxVal ? <span className="font-medium text-[14px] leading-5.5 text-dark-grey ">Разовый максимум: {maxVal}кг</span> : ''}
+                { maxVal ? <span className="font-medium text-[14px] leading-5.5 text-grey-dark ">Разовый максимум: {maxVal}кг</span> : ''}
             </div>
             <Image src="/svg/open_arrow.svg" alt="open" width={22} height={22} className="self-center" />
         </div>
